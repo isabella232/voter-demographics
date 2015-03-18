b4e7a22dd086fcd98e162b89706fb92ad1502fa0
Christopher Groskopf <staringmonkey@gmail.com>
2015-03-18 11:29:13 -0400

    Clean up column names.

--- make_readable.py
+++ make_readable.py
@@ -20,34 +20,34 @@ FIPS = { v: k for k, v in STATES.items() }
 FIELD_MAPPINGS = OrderedDict([
     ('gestfips', FIPS),
     ('ptdtrace', {
-        '1': 'White Only'
-        '2': 'Black Only'
-        '3': 'American Indian, Alaskan Native Only'
-        '4': 'Asian Only'
-        '5': 'Hawaiian/Pacific Islander Only'
-        '6': 'White-Black'
-        '7': 'White-AI'
-        '8': 'White-Asian'
-        '9': 'White-HP'
-        '10': 'Black-AI'
-        '11': 'Black-Asian'
-        '12': 'Black-HP'
-        '13': 'AI-Asian'
-        '14': 'AI-HP'
-        '15': 'Asian-HP'
-        '16': 'W-B-AI'
-        '17': 'W-B-A'
-        '18': 'W-B-HP'
-        '19': 'W-AI-A'
-        '20': 'W-AI-HP'
-        '21': 'W-A-HP'
-        '22': 'B-AI-A'
-        '23': 'W-B-AI-A'
-        '24': 'W-AI-A-HP'
-        '25': 'Other 3 Race Combinations'
+        '1': 'White Only',
+        '2': 'Black Only',
+        '3': 'American Indian, Alaskan Native Only',
+        '4': 'Asian Only',
+        '5': 'Hawaiian/Pacific Islander Only',
+        '6': 'White-Black',
+        '7': 'White-AI',
+        '8': 'White-Asian',
+        '9': 'White-HP',
+        '10': 'Black-AI',
+        '11': 'Black-Asian',
+        '12': 'Black-HP',
+        '13': 'AI-Asian',
+        '14': 'AI-HP',
+        '15': 'Asian-HP',
+        '16': 'W-B-AI',
+        '17': 'W-B-A',
+        '18': 'W-B-HP',
+        '19': 'W-AI-A',
+        '20': 'W-AI-HP',
+        '21': 'W-A-HP',
+        '22': 'B-AI-A',
+        '23': 'W-B-AI-A',
+        '24': 'W-AI-A-HP',
+        '25': 'Other 3 Race Combinations',
         '26': 'Other 4 and 5 Race Combinations'
     }), ('pehspnon', {
-        '1': 'Hispanic'
+        '1': 'Hispanic',
         '2': 'Non-Hispanic'
     }), ('pesex', {
         '1': 'Male',
@@ -55,16 +55,16 @@ FIELD_MAPPINGS = OrderedDict([
     }), ('prtage', {
         '80': '80-84'
     }), ('pes1', {
-        '1': 'Yes'
-        '2': 'No'
-        '-2': 'Don\'t Know'
-        '-3': 'Refused'
+        '1': 'Yes',
+        '2': 'No',
+        '-2': 'Don\'t Know',
+        '-3': 'Refused',
         '-9': "No Response"
     }), ('pes2', {
-        '1': 'Yes'
-        '2': 'No'
-        '-2': 'Don\'t Know'
-        '-3': 'Refused'
+        '1': 'Yes',
+        '2': 'No',
+        '-2': 'Don\'t Know',
+        '-3': 'Refused',
         '-9': "No Response"
     }), ('pes3', {
         '1': 'Missed deadline',
@@ -132,22 +132,44 @@ FIELD_MAPPINGS = OrderedDict([
     })
 ])
 
+OUTPUT_HEADERS = OrderedDict([
+    ('gestfips', 'state'),
+    ('ptdtrace', 'race'),
+    ('pehspnon', 'hispanic'),
+    ('pesex', 'sex'),
+    ('prtage', 'age'),
+    ('pes1', 'voted'),
+    ('pes2', 'registered'),
+    ('pes3', 'why_not_registered'),
+    ('pes4', 'why_not_vote'),
+    ('pes5', 'voted_how'),
+    ('pes6', 'voted_on_election_day'),
+    ('pes7', 'registered_how'),
+    ('pes8', 'lived_at_current_address')
+])
+
 output = []
 
 with open('data.csv') as f:
     reader = csv.DictReader(f)
 
     for row in reader:
+        output_row = {}
+
         for column, mapping in FIELD_MAPPINGS.items():
             data = row[column]
 
             if data in mapping:
-                row[column] = mapping[data]
+                value = mapping[data]
+            else:
+                value = data
 
-        output.append(row)
+            output_row[OUTPUT_HEADERS[column]] = value
+
+        output.append(output_row)
 
 with open('simple.csv', 'w') as f:
-    writer = csv.DictWriter(f, fieldnames=FIELD_MAPPINGS.keys())
+    writer = csv.DictWriter(f, fieldnames=OUTPUT_HEADERS.values())
 
     writer.writeheader()
     writer.writerows(output)
